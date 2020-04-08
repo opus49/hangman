e6f52aab7f22a2acb653ec2b79cbbeae6fa755f7
Mike Puskar <puskar49@gmail.com>
2020-04-08 16:37:52 +0000

    Added unit tests

--- tests/test_screen.py
+++ tests/test_screen.py
@@ -40,6 +40,25 @@ class TestScreen(unittest.TestCase):
                 hangman.cli.screen.Screen.gallows(index)
                 mock_print.assert_called_with(hangman.cli.screen._GALLOWS[-1])
 
+    def test_get(self):
+        """Test the get method"""
+        with mock.patch("builtins.input") as mock_input:
+            mock_input.return_value = "foo"
+            self.assertEqual(hangman.cli.screen.Screen.get("Foo?"), "foo")
+
+    def test_put(self):
+        """Test the put method"""
+        with mock.patch("builtins.print") as mock_print:
+            hangman.cli.screen.Screen.put("foo!")
+            mock_print.assert_called_with("\nfoo!")
+
+    def test_goodbye(self):
+        """Test the goodbye method"""
+        with mock.patch("builtins.print") as mock_print:
+            hangman.cli.screen.Screen.goodbye()
+            output = ",".join([str(x) for x in mock_print.call_args_list])
+        self.assertTrue("Goodbye" in output)
+
 
 if __name__ == "__main__":
     unittest.main()
